e423681765fbdfea22f86df179d2dba6e16ebf54
gonzaloangaut <angaut13@gmail.com>
2026-09-19 22:35:38 -0300

    Refactoring api

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
-from fastapi import FastAPI
-
+from app.api import create_app
 from app.chatbot.service import MedicalAssistance
-from app.schemas import ChatRequest
 from app.chatbot.llm import QwenLLM
 from app.chatbot.retrieval import SemanticRetriever
 from app.chatbot.knowledge import load_knowledge
-
-# Create the APP
-app = FastAPI()
 
 # Load the LLM
 llm = QwenLLM()
@@ -25,16 +20,5 @@ bot = MedicalAssistance(
     retriever=retriever,
 )
 
-# Get to check the status of the service
-@app.get("/health")
-def health():
-    return {"status": "ok"}
-
-# Post petition
-@app.post("/predict")
-def predict(request: ChatRequest):
-    chat_history_dicts = [m.model_dump() for m in request.messages]
-    # Get the answer from the bot
-    response = bot.generate_response(chat_history_dicts)
-
-    return {"response": response}
+# Create the FastAPI app
+app = create_app(bot)
